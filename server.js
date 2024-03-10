0ed822caaf4a9fe535f418d2675c8e79e0546f20
shefyg <shefyg@gmail.com>
2024-03-10 14:49:03 +0200

    Added first card page example

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   console.log("Request received");
-  res.status(500).json({ message: "Server error" });
-  res.send("Hi there! 2");
+
+  //// status and message:
+  //res.status(200).send("Hi there! 3");
+
+  //// download file
+  // res.download("server.js");
+
+  //// render template or page
+  res.render("CardExamplePage", {
+    fullname: "Shefy Gur-Ary",
+    title: "Software Developer / Tech Lead",
+    email: "shefyg@gmail.com",
+    linkedin: "https://www.linkedin.com/in/shefy-gur-ary/",
+    phone: "0541234567",
+    github: "https://github.com/shefyg",
+  });
 });
 
 app.listen(3000);
